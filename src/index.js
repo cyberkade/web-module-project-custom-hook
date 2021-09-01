080480f991bc955227cb42977a8131a6f23c830e
Kade Griffith <kadeg95@gmail.com>
2021-09-01 12:15:27 -0600

    added chart color change depending on theme as well as a little more information added from the api request. final commit

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useDarkMode(false);
+  const [darkMode, setDarkMode] = useDarkMode('dark-mode', false);
 
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&price_change_percentage=1y"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=true&price_change_percentage=1y"
       )
       .then(res => {
         setCoinData(res.data)
@@ -29,7 +29,7 @@ const App = () => {
     {!coinData && <h1 className='loading'>Loading</h1>}
     <div className={darkMode ? "dark-mode App" : "App"}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
-      <Charts coinData={coinData} />
+      <Charts coinData={coinData} darkMode={darkMode} />
     </div>
     </>
   );
